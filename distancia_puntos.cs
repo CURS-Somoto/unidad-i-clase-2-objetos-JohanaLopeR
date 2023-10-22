81d094682e55e2636d3731560d7b3216ce617070
JohanaLopeR <147268181+JohanaLopeR@users.noreply.github.com>
2023-10-21 18:00:36 -0600

    Update distancia_puntos.cs
    Entrega de trabajo

--- distancia_puntos.cs
+++ distancia_puntos.cs
@@ -1,1 +1,59 @@
 
+using System;
+
+class Punto
+{
+    public decimal X { get; set; }
+    public decimal Y { get; set; }
+    public decimal Z { get; set; }
+
+    public Punto(decimal x, decimal y)
+    {
+        X = x;
+        Y = y;
+        Z = 0; 
+    }
+
+    public Punto(decimal x, decimal y, decimal z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    public double MedirDistancia(Punto destino)
+    {
+        double distancia;
+
+        if (Z == 0 && destino.Z == 0)
+        {
+            distancia = Math.Sqrt(Math.Pow((double)(destino.X - X), 2) + Math.Pow((double)(destino.Y - Y), 2));
+        }
+        else 
+        {
+            distancia = Math.Sqrt(Math.Pow((double)(destino.X - X), 2) + Math.Pow((double)(destino.Y - Y), 2) + Math.Pow((double)(destino.Z - Z), 2));
+        }
+
+        return distancia;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Punto puntoOrigen2D = new Punto(0, 0);
+        Punto puntoDestino2D = new Punto(3, 4);
+
+        double distancia2D = puntoOrigen2D.MedirDistancia(puntoDestino2D);
+
+        Console.WriteLine("Distancia en 2D: " + distancia2D);
+
+        Punto puntoOrigen3D = new Punto(1, 2, 3);
+        Punto puntoDestino3D = new Punto(4, 5, 6);
+
+        double distancia3D = puntoOrigen3D.MedirDistancia(puntoDestino3D);
+
+        Console.WriteLine("Distancia en 3D: " + distancia3D);
+    }
+}
